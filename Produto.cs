02e9e70f8bd87c2f1db239da3a98a57149c7ceca
sena-code <sena.carbone@gmail.com>
2020-06-29 09:52:00 -0300

    without challenger

---
+++ Produto.cs
@@ -0,0 +1,37 @@
+using System.IO;
+
+namespace create_data_in_excel
+{
+    public class Produto
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public float Cost { get; set; }
+        private const string PATH = "Database/produto.csv";
+        private string LinePrepare(Produto p)
+        {
+            return $"codigo={p.Code}; nome={p.Name}; preço={p.Cost}";
+        }
+        public void Register(Produto prod)
+        {
+            var line = new string[] { LinePrepare(prod) };
+
+            File.AppendAllLines(PATH, line);
+        }
+
+        public Produto ()
+        {
+            //the file is create if does not exist
+            if(!File.Exists(PATH)){
+                File.Create(PATH).Close();
+
+            }
+        }
+        public Produto (int _code, string _name, float _cost)
+        {
+            this.Code = _code;
+            this.Name = _name;
+            this.Cost = _cost;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace create_data_in_excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Code = 1;
+            p1.Name = "Keyboard & Mouse";
+            p1.Cost = 597.8f;
+
+            p1.Register(p1);
+        }
+    }
+}
