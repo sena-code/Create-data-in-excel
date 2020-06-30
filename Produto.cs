1604e73dd1e2c1f60996e9b836f7610d539e6ea5
sena-code <sena.carbone@gmail.com>
2020-06-30 10:44:10 -0300

    All done

--- Produto.cs
+++ Produto.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.IO;
+using System;
+using System.Linq;
 
 namespace create_data_in_excel
 {
@@ -9,16 +12,71 @@ namespace create_data_in_excel
         public float Cost { get; set; }
         private const string PATH = "Database/produto.csv";
         private const string PATHDIRECTORY = "Database";
+
+        /// <summary>
+        /// Preparing the lines
+        /// </summary>
+        /// <param name="p">for put itens on console</param>
+        /// <returns>itens</returns>
         private string LinePrepare(Produto p)
         {
-            return $"codigo={p.Code}; nome={p.Name}; preço={p.Cost}";
+            return $"code={p.Code}; name={p.Name}; cost={p.Cost}";
         }
+        /// <summary>
+        /// Registre product 
+        /// </summary>
+        /// <param name="prod">to register a product</param>
         public void Register(Produto prod)
         {
             var line = new string[] { LinePrepare(prod) };
 
             File.AppendAllLines(PATH, line);
         }
+
+        /// <summary>
+        /// Read products 
+        /// </summary>
+        /// <returns>products with spaces</returns>
+
+        public List<Produto> Read()
+        {
+            //create the list for get the return 
+            List<Produto> prod = new List<Produto>();
+            //Read the .csv and add a array lines
+            string[] lines = File.ReadAllLines(PATH);
+
+            //sweep our lines 
+            foreach(string line in lines )
+            {
+                //code=1;name=Keybord and Mouse;cost=579.8
+                string[] data = line.Split(";"); 
+
+                Produto p = new Produto();
+                p.Code = Int32.Parse(Separate(data[0]));
+                p.Name = Separate(data[1]);
+                p.Cost = float.Parse( Separate(data[2]));
+
+                prod.Add(p);
+                
+            } return prod;
+
+            
+
+        }
+
+        /// <summary>
+        /// To separate itens after "="
+        /// </summary>
+        /// <param name="data">csv colum has been separate</param>
+        /// <returns>string only with column's value</returns>
+    
+         public string Separate(string data)
+            {
+                return data.Split("=")[1];
+            }
+            /// <summary>
+            /// To create a Directory or a File if they doesnt exist
+            /// </summary>
 
         public Produto ()
         {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace create_data_in_excel
 {
@@ -6,12 +7,42 @@ namespace create_data_in_excel
     {
         static void Main(string[] args)
         {
+            List<Produto> produtos = new List<Produto>();
             Produto p1 = new Produto();
             p1.Code = 1;
             p1.Name = "Keyboard & Mouse";
             p1.Cost = 597.8f;
 
             p1.Register(p1);
+
+             Produto p2 = new Produto();
+            p1.Code = 2;
+            p1.Name = "Things";
+            p1.Cost = 598.8f;
+
+            p1.Register(p1);
+
+            List<Produto> list = p1.Read();
+
+            foreach(Produto iten in list)
+            {
+                Console.WriteLine($"R$ {iten.Cost} - {iten.Name}");
+            }
+
+            
+
+            var name = list.Find(x => x.Name == "Things");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(name.Cost);
+            Console.ResetColor();
+            var key = list.Find(x => x.Name == "Keyboard & Mouse");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(key.Name);
+            Console.ResetColor();
+                
+
+
+             
         }
     }
 }
